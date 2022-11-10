cb0db68187fe65b421d4a8feaf587ea74fc5a519
Pratik Kabade <pratikkabade@hotmail.com>
2022-11-10 22:26:07 +0530

    React

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import './style/App.css';
+import { Header } from './components/Header';
+import { Home } from './pages/Home';
+
+function App() {
+  return (
+    <div className='App'>
+      <Header />
+      <Home />
+    </div>
+  );
+}
+
+export default App;
